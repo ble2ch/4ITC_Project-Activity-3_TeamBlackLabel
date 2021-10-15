b5ccf82934e6a2f0712819907bf19897239a6c1a
Raymark Ramos <60497150+ble2ch@users.noreply.github.com>
2021-10-15 03:59:14 +0000

    First Commit

---
+++ mapquest_parse-json_7.py
@@ -0,0 +1,44 @@
+import urllib.parse
+import requests
+import colorama
+from colorama import Fore
+from colorama import Style
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "Rigu6LhDU2Utq3Y1UBoZMzPUhcE1yzTG"
+
+while True: 
+    user_name = input(Fore.RESET + "Name of the user: ") 
+    if user_name == "quit" or user_name == "q": 
+        break
+    orig = input(Fore.RESET + "Starting Location: ") 
+    if orig == "quit" or orig == "q": 
+        break
+    dest = input(Fore.RESET + "Destination: ") 
+    if dest == "quit" or dest == "q": 
+        break
+        
+    url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+    print("URL: " + (url))
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+    if json_status == 0:
+        print(Fore.GREEN + "Hello " + user_name + "! Here are your directions: ")
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+            print(Fore.GREEN + (each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+            print('Extra Information: ')
+            print("=============================================\n") 
+        print("Fuel Used The Entire Trip: " + str(json_data["route"]["fuelUsed"]))
+    elif json_status == 402: 
+        print("**********************************************") 
+        print(Fore.RED + "Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.") 
+        print("**********************************************\n") 
+    elif json_status == 611: 
+        print("**********************************************") 
+        print(Fore.RED + "Status Code: " + str(json_status) + "; Missing an entry for one or both locations.") 
+        print("**********************************************\n")
+    else:
+        print("************************************************************************") 
+        print(Fore.RED + "For Staus Code: " + str(json_status) + "; Refer to:") 
+        print("https://developer.mapquest.com/documentation/directions-api/status-codes") 
+        print("************************************************************************\n")
